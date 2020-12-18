5ac335c3db4277d41ad71a4069e748c01ef0f9ca
georgicg79 <georginacornet@gmail.com>
2020-12-18 17:12:14 +0100

    - Afegits bottomBar amb les icones i els diferents widgets. - Creades les diferents pantalles dins la carpeta screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'screens/globalCalendarScreen.dart';
 import 'sign_in_flow/auth_state_switch.dart';
 
 void main() async {
@@ -10,69 +8,7 @@ void main() async {
   await Firebase.initializeApp();
   runApp(
     AuthStateSwitch(
-      app: MyApp(),
+      app: GlobalCalendarScreen(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GimnàsApp test xavi',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
-    final act = FirebaseFirestore.instance.collection('Activitats');
-    return StreamBuilder(
-      stream: act.snapshots(),
-      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        final docs = snapshot.data.docs;
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("Gimnas App"),
-            actions: [
-              IconButton(
-                icon: Icon(Icons.logout),
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-              ),
-            ],
-          ),
-          body: Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  itemCount: docs.length,
-                  itemBuilder: (context, index) {
-                    final item = docs[index];
-                    return ListTile(
-                      title: Text(
-                        item['tipus'],
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Text("${user.email}"),
-              Text("${user.uid}"),
-            ],
-          ),
-        );
-      },
-    );
-  }
 }
---
+++ lib/screens/globalCalendarScreen.dart
@@ -0,0 +1,136 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class GlobalCalendarScreen extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GimnàsApp test xavi',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int screen = 1;
+  /*
+    0 -> newsScreen
+    1 -> CalendarGlobal
+    2 -> personalCalendar
+    */
+  @override
+  Widget build(BuildContext context) {
+    final act = FirebaseFirestore.instance.collection('Activitats');
+    return StreamBuilder(
+      stream: act.snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final docs = snapshot.data.docs;
+        var mainwidget;
+        if (screen == 0) {
+          mainwidget = Container(
+            child: Text("0"), //Aquí anirà el widget del llistat de les notícies
+          );
+        } else if (screen == 1) {
+          //Aquí anirà el widget del calendari global
+          mainwidget = ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              final item = docs[index];
+              return ListTile(
+                title: Text(
+                  item['tipus'],
+                ),
+              );
+            },
+          );
+        } else {
+          mainwidget =
+              Container(); //Aquí anirà el widget del calendari personal
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Gimnas App"),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.logout),
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+              ),
+            ],
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: mainwidget,
+                ),
+                BottomAppBar(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8),
+                        topRight: Radius.circular(8),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Spacer(),
+                          FlatButton(
+                            child: Icon(Icons.list),
+                            onPressed: () {
+                              setState(() {
+                                screen = 0;
+                              });
+                            },
+                          ),
+                          Spacer(),
+                          FlatButton(
+                            child: Icon(Icons.calendar_today),
+                            onPressed: () {
+                              setState(() {
+                                screen = 1;
+                              });
+                            },
+                          ),
+                          Spacer(),
+                          FlatButton(
+                            child: Icon(Icons.perm_contact_calendar),
+                            onPressed: () {
+                              setState(() {
+                                screen = 2;
+                              });
+                            },
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
