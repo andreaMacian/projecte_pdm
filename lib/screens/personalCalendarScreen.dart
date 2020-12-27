aed7766add8d36016b9d2f3fea98559b50186e6f
Xavier Gaya <xavier.gaya@estudiantat.upc.edu>
2020-12-27 17:58:27 +0100

    Ampliacio de codi de personalScreen2

--- lib/screens/personalCalendarScreen.dart
+++ lib/screens/personalCalendarScreen.dart
@@ -3,12 +3,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
-
-
-
-
-
 class PersonalCalendarScreen extends StatelessWidget {
   const PersonalCalendarScreen({
     Key key,
@@ -19,13 +13,27 @@ class PersonalCalendarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final act = FirebaseFirestore.instance.collection(
+        'Activitats'); //recollim dades de firebase amb les activitats
 
-  final act = FirebaseFirestore.instance.collection('Activitats'); //recollim dades de firebase amb les activitats
-  final ins = FirebaseFirestore.instance.collection('Usuaris').doc('c5Dz89sXkUZ7s77yI8pdPQ6s0Nz1').collection('inscripcions');
-  
-  
+    final ins = FirebaseFirestore.instance
+        .collection('Usuaris')
+        .doc(
+            'c5Dz89sXkUZ7s77yI8pdPQ6s0Nz1') //preguntar com posar aqui el usuari que esta amb login?????????????
+        .collection(
+            'inscripcions'); //recollim les dades de les inscripcions , en aquest cas nomes d`un usuari
 
-
+    /*  StreamBuilder(     //NO SE COM rescatar els valor de les dos llistes per encreuar-los despres.
+      stream: ins.snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final insDocs = snapshot.data.docs;
+      },
+    ); */
 
     return StreamBuilder(
       stream: act.snapshots(),
@@ -35,49 +43,26 @@ class PersonalCalendarScreen extends StatelessWidget {
             child: CircularProgressIndicator(),
           );
         }
-  
-        
-  
+
         final actDocs = snapshot.data.docs;
-          return ListView.builder(
-         
+        return ListView.builder(
           itemCount: actDocs.length,
           itemBuilder: (context, index) {
             final item = actDocs[index];
-               for (int i =0; i< actDocs.length ;i++){
-                 if (actDocs.id == ins.id){ }
-               }
+            /* for (int i = 0; i < actDocs.length; i++) { //// filtre per encreuar les dades de les 2 coleccions
+              for (int j = 0; j < actDocs.length; j++) {
+                if (actDocs[i].id == actDocs[j].id) {
+                  final item = actDocs[i].id;
+                  
+                }
+              }
+            } */
             return ListTile(
-              title:Text(item.id)
+              title: Text(item.id),
             );
           },
-        ); 
+        );
       },
     );
-
-/* return StreamBuilder(
-      stream: ins.snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        final insDocs = snapshot.data.docs;
-          return ListView.builder(
-          itemCount: insDocs.length,
-          itemBuilder: (context, index) {
-            final itemins = insDocs[index];
-            return ListTile(
-              title:Text(itemins.id)
-            );
-          },
-        ); 
-      },
-    ); */
-
-
-
-
   }
 }
