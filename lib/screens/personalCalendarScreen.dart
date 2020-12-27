abc2e4b1e3efc1f302b4c1385efaab9737f2653e
Xavier Gaya <xavier.gaya@estudiantat.upc.edu>
2020-12-27 17:24:44 +0100

    Ampliacio de codi de personalScreen

--- lib/screens/personalCalendarScreen.dart
+++ lib/screens/personalCalendarScreen.dart
@@ -1,7 +1,12 @@
+//import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-final user = FirebaseFirestore.instance.collection('Usuaris');
+
+
+
+
 
 
 class PersonalCalendarScreen extends StatelessWidget {
@@ -14,17 +19,65 @@ class PersonalCalendarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.yellow)/*ListView.builder(
-      //Aquí anirà el widget del calendari personal
-      itemCount: docs.length,
-      itemBuilder: (context, index) {
-        final item = docs[index];
-        return ListTile(
-          title: Text(
-            item['tipus'],
-          ),
-        );
+
+  final act = FirebaseFirestore.instance.collection('Activitats'); //recollim dades de firebase amb les activitats
+  final ins = FirebaseFirestore.instance.collection('Usuaris').doc('c5Dz89sXkUZ7s77yI8pdPQ6s0Nz1').collection('inscripcions');
+  
+  
+
+
+
+    return StreamBuilder(
+      stream: act.snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+  
+        
+  
+        final actDocs = snapshot.data.docs;
+          return ListView.builder(
+         
+          itemCount: actDocs.length,
+          itemBuilder: (context, index) {
+            final item = actDocs[index];
+               for (int i =0; i< actDocs.length ;i++){
+                 if (actDocs.id == ins.id){ }
+               }
+            return ListTile(
+              title:Text(item.id)
+            );
+          },
+        ); 
       },
-    )*/;
+    );
+
+/* return StreamBuilder(
+      stream: ins.snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final insDocs = snapshot.data.docs;
+          return ListView.builder(
+          itemCount: insDocs.length,
+          itemBuilder: (context, index) {
+            final itemins = insDocs[index];
+            return ListTile(
+              title:Text(itemins.id)
+            );
+          },
+        ); 
+      },
+    ); */
+
+
+
+
   }
 }
